77bc6afcca56961c110c466902e11eec6072b7bb
Victor Tran <victor-likes-coding@tempestas.io>
2023-03-21 21:57:37 -0400

    write queries (WIP)

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,22 +1,29 @@
+const db = require('../../data/db-config');
+
 const getAll = () => {
   // DO YOUR MAGIC
-}
+  return db('accounts');
+};
 
-const getById = id => {
+const getById = (id) => {
   // DO YOUR MAGIC
-}
+  return db('accounts').where({ id }).first();
+};
 
-const create = account => {
+const create = (account) => {
   // DO YOUR MAGIC
-}
+  return db('accounts').insert(account).first();
+};
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
-}
+  return db('accounts').where({ id }).insert(account).first();
+};
 
-const deleteById = id => {
+const deleteById = (id) => {
   // DO YOUR MAGIC
-}
+  return db('accounts').where({ id }).del();
+};
 
 module.exports = {
   getAll,
@@ -24,4 +31,4 @@ module.exports = {
   create,
   updateById,
   deleteById,
-}
+};
